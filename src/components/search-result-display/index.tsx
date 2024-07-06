bb56e09727244ed40e5282464340c6949150cc8d
Mideli <muzipovanastia@gmail.com>
2024-07-06 20:06:00 +0300

    feat: add max width for cards container

--- src/components/search-result-display/index.tsx
+++ src/components/search-result-display/index.tsx
@@ -64,7 +64,7 @@ export class SearchRequestDisplay extends Component<Props, State> {
         {response?.data.length === 0 ? (
           <p>No cards were found</p>
         ) : (
-          <ul className="flex flex-row flex-wrap gap-4 justify-center p-3">
+          <ul className="flex flex-row flex-wrap max-w-screen-2xl gap-4 justify-center p-3">
             {response?.data.map((pokemon) => (
               <li key={pokemon.id}>
                 <PokemonCard {...pokemon} />
